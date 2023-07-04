b1c75e38862aa54562e16737bc6f60ef02440a09
vedikamalviya <vedikamalviya@gmail.com>
2023-07-04 12:13:08 +0530

    added logger

---
+++ Backend/exceptions/httpException.js
@@ -0,0 +1,11 @@
+
+ class HttpException extends Error {
+  constructor(status, message) {
+    super(message);
+    this.status = status;
+    this.message = message;
+  }
+}
+
+module.exports = HttpException;
+
--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,16 @@ const dotenv=require('dotenv').config()
 const colors= require('colors')
 const {errorHandler}= require('./middleware/errorMiddleware')
 const connectDB=require('./config/db')
+const { logger, stream } = require("./utils/logger.utils");
+const morgan = require("morgan");
+
+
+
+
 const port=process.env.PORT ||5000
+
+
+
 //console.log(PORT)
 connectDB()
 const app= express()
@@ -12,4 +21,11 @@ app.use(express.urlencoded({extended: false}))
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/user', require('./routes/userRoutes'))
 app.use(errorHandler)
+
+
+const initializeMiddlewares = ()=>{
+    app.use(morgan('dev',{ stream }));
+}
+
+initializeMiddlewares();
 app.listen(port, ()=> console.log(`app listening on port ${port}`))
